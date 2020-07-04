eaba69fb49d79f2e8842c9d8db8c6d4f37a259fe
yaronyes <yaronyes@hotmail.com>
2020-07-04 12:23:10 +0300

    Added an interval to replace the picture every 5 seconds for the bottom middle picture. The code written in ES6 syntax.

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
-const images = [];
+const images = ["paz_250x110103131473.jpg", "banner_atar.jpg", "250X110571325734.jpg"];
+const imageFolderPath = "../../img/";
+const $image = document.getElementById("changing-img");
+var index = -1;
+
+const getNextIndex = (index) => {
+    return index < 2  ? index + 1 : 0;
+}
+
+setInterval(() => {
+    index = getNextIndex(index);        
+    $image.src = `${imageFolderPath}${images[getNextIndex(index)]}`;
+}, 5000);
